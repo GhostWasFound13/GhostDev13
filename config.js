796cc32e7d933af58fac9681ebe732df6ce20aa4
Ghost <87599595+GhostWasFound13@users.noreply.github.com>
2025-07-01 12:48:07 +0700

    Create config.js

---
+++ config.js
@@ -0,0 +1,24 @@
+// config.js
+const discordProfile = {
+  userId: "YOUR_USER_ID",
+  avatarHash: "YOUR_AVATAR_HASH",
+  username: "ghostrgh54",
+  badges: [
+    {
+      src: "https://raw.githubusercontent.com/discordjs/discord.js/main/website/static/img/logo.svg",
+      title: "Verified"
+    },
+    {
+      src: "https://cdn-icons-png.flaticon.com/512/252/252025.png",
+      title: "Early Supporter"
+    }
+  ]
+};
+
+const socialLinks = {
+  github: "https://github.com/ghostdev13",
+  twitter: "https://x.com/ghostdev13",
+  roblox: "https://roblox.com/users/ghostdev13",
+  twitch: "https://twitch.tv/ghostdev13",
+  youtube: "https://youtube.com/@ghostdev13"
+};
